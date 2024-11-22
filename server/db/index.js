e78c53c7e8b3dc8e74d4db2d5d5476893f7fdbdb
Siddharth Jain <125799102+jsiddharth847@users.noreply.github.com>
2024-11-22 12:37:34 +0530

    Update index.js

--- server/db/index.js
+++ server/db/index.js
@@ -3,11 +3,9 @@ const { DB_NAME } = require("../constants");
 
 const connectDB = async () => {
   try {
-    const connectionInstance = await mongoose.connect(
-      process.env.MONGODBURI
-    );
+   
 
-    // const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+    const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
     console.log(
       `\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`
     );
