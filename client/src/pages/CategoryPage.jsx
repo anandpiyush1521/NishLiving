0737b3a0cf16b27d5682521ba82034e11bb2b57b
Siddharth Jain <125799102+jsiddharth847@users.noreply.github.com>
2024-11-28 11:23:21 +0530

    Update CategoryPage.jsx
    changing the url

--- client/src/pages/CategoryPage.jsx
+++ client/src/pages/CategoryPage.jsx
@@ -18,7 +18,7 @@ const CategoryPage = () => {
   const getFeedListings = async () => {
     try {
       const response = await fetch(
-          `http://localhost:3001/properties?category=${category}`,
+          `https://nish-living.vercel.app/properties?category=${category}`,
         {
           method: "GET",
         }
